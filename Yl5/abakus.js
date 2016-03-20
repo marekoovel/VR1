e9a6e4d7255788550df05b36ef8ec6908a0cfbf2
Marek Öövel <Marek.Oovel@ee.abb.com>
2016-03-20 23:44:10 +0200

    Ylesanne 5

--- Yl5/abakus.js
+++ Yl5/abakus.js
@@ -1,20 +1,14 @@
 window.onload = function() {
 	var beads = document.getElementsByClassName('bead');
+	
+	for (i = 0; i < beads.length; i++) {
+		beads[i].onclick = floatChanging;
+	}
+	
 	function floatChanging(pearl) {
-		if (pearl.target.style.cssFloat == "null" || pearl.target.style.cssFloat == "") {
-			if (pearl.target.className == "bead beadleft") {
-				pearl.target.style.cssFloat = "right";
-		    } else {
-		    	pearl.target.style.cssFloat = "left";
-		    }
-		} else 
 			if (pearl.target.style.cssFloat == "left") {
 	    		pearl.target.style.cssFloat = "right";
 		    } else {
 		    	pearl.target.style.cssFloat = "left";
 		    }
-		}
-	for (i = 0; i < beads.length; i++) {
-		beads[i].onclick = floatChanging;
-	}
-}
+}}
